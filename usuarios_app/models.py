59be0200391bb0e8baeb05174abe264976a5b8ad
Jothanf <jothanferney@gmail.com>
2024-08-14 17:14:25 -0500

    primera tarea de base de datos

--- usuarios_app/models.py
+++ usuarios_app/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 
 # Create your models here.
- 
-
-class UsuarioModel(models.Model):
-    nombre = models.CharField(max_length=50)
-    telefono = models.CharField(max_length=20, blank=True, null=True)
-    direccion = models.CharField(max_length=255, blank=True, null=True)
+class UserModel(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    document_number = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    role = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
-        return self.nombre
+        return self.name
+
+    
+"""
+trigger: active cuando cuando se visite el home => disparar un contador
+trigger: active cuando cuando se haga un login => disparar un contador
+
+
+
+usuariovisitante
+    usuario
+    contraseña
+    contador
+
+usuarioregistrado
+    definir campo
+
+usuarioconsultor
+    definir campo
+
+superadmin
+    definir campos
+
+"""
